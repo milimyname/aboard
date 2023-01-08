b5910a6e9cba5ba28cf55a318f6b6b0cf9a6a51b
mili <kamil.maksudov2019@gmail.com>
2023-01-08 15:33:20 +0100

    feat: header ui init

---
+++ src/components/header.svelte
@@ -0,0 +1,33 @@
+<script>
+	// your script goes here
+	import { ArrowDown, Search, Command } from 'lucide-svelte';
+	import { Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+<header class="py-6 flex justify-between">
+	<div class="flex gap-8 items-center">
+		<span class="text-6xl">🦊</span>
+		<h1 class="text-4xl">Header</h1>
+		<ArrowDown size={20} />
+	</div>
+	<!-- Search -->
+	<div class="flex items-center relative w-1/4">
+		<Search size={16} class="absolute left-4" />
+		<input
+			type="text"
+			placeholder="Search"
+			class="bg-slate-100 rounded-full p-2  w-full enabled:pl-10"
+		/>
+		<div class="absolute right-4 flex gap-1 items-center">
+			<Command size={16} />
+			<span>K</span>
+		</div>
+	</div>
+	<!-- Active users -->
+	<div class="flex">
+		<Avatar initials="JD" class=" bg-yellow-300" />
+		<Avatar initials="JD" class=" bg-yellow-300" />
+		<Avatar initials="JD" class=" bg-yellow-300" />
+		<Avatar initials="JD" class=" bg-yellow-300" />
+	</div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,16 @@
 	import { AppShell } from '@skeletonlabs/skeleton';
 	import Sidebar from '@components/sidebar.svelte';
 	import Footer from '@components/footer.svelte';
+	import Header from '@components/header.svelte';
 </script>
 
 <AppShell>
 	<svelte:fragment slot="sidebarLeft">
 		<Sidebar />
 	</svelte:fragment>
-	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
-	<!-- Router Slot -->
+	<svelte:fragment slot="pageHeader">
+		<Header />
+	</svelte:fragment>
 	<slot />
 	<!-- ---- / ---- -->
 	<svelte:fragment slot="pageFooter">
